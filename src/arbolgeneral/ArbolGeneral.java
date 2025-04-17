03551cbe1d1ce75982cd44d91e5d4a63c0722130
Pablo Alcolea Sesse <128633352+PabloAlcoleaSesse@users.noreply.github.com>
2025-04-17 13:13:47 +0200

    Final

--- src/arbolgeneral/ArbolGeneral.java
+++ src/arbolgeneral/ArbolGeneral.java
@@ -149,7 +149,6 @@ public class ArbolGeneral {
 
             System.out.println("--------");
             System.out.println(grado(e));
-            System.out.println(contarHijosPorEliminacion(b));
 
             System.out.println("--------");
 
@@ -158,7 +157,7 @@ public class ArbolGeneral {
 
     }
 //_________________________________________________________________________________________
-    //Primer metodo para contar los hijos de un arbol
+
 
     public static int grado(ArbolGeneral arbol) {
         if (arbol == null || arbol.esVacio()) {
@@ -178,7 +177,6 @@ public class ArbolGeneral {
             return 0;
         }
 
-        // Contar número de hijos del nodo actual
         int contador = 0;
         NodoGeneral hijo = nodo.primerHijo;
         while (hijo != null) {
@@ -186,7 +184,7 @@ public class ArbolGeneral {
             hijo = hijo.hermano;
         }
 
-        // Recorremos recursivamente cada hijo
+
         int maxGradoHijos = 0;
         hijo = nodo.primerHijo;
         while (hijo != null) {
@@ -196,30 +194,4 @@ public class ArbolGeneral {
 
         return Math.max(contador, maxGradoHijos);
     }
-//_________________________________________________________________________________________
-   //Segundo metodo para contar los hijos de un arbol
-    public static int contarHijosPorEliminacion(ArbolGeneral arbol) {
-
-        if (arbol == null || arbol.esVacio()) {
-            return 0;
-        }
-
-        try {
-
-            ArbolGeneral copia = new ArbolGeneral(arbol.raiz);
-            ArbolGeneral hijo = copia.primerHijo();
-
-            if (hijo.esVacio()) {
-                return 0;
-            }
-
-            copia.eliminar(hijo);
-
-            return 1 + contarHijosPorEliminacion(copia);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return 0;
-        }
-    }
-
 }
